6815dfc159a34be5b2037a2aa93178b790f99d12
hermann <hkamguin@gmail.com>
2024-11-22 18:01:56 +0100

    progress

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -41,7 +41,7 @@ const routes: Routes = [
 
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, { scrollPositionRestoration: 'top' })],
+  imports: [RouterModule.forRoot(routes, { scrollPositionRestoration: 'disabled' })],
   exports: [RouterModule],
 })
 export class AppRoutingModule { }
